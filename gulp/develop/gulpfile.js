e98c54231008d1fde1d6546c66443ad6113c6918
wangerniu <596864509@qq.com>
2017-07-10 13:38:35 +0800

    submit for gulp work

--- gulp/develop/gulpfile.js
+++ gulp/develop/gulpfile.js
@@ -8,47 +8,48 @@ const gulpHtmlMin = require('gulp-htmlmin');
 const gulpBabel = require('gulp-babel');
 const gulpEs2015 = require('babel-preset-es2015');
 const gulpWebpack =require('gulp-webpack');
+const gulpWatch = require('gulp-watch');
+const gulpLiveReload = require('gulp-livereload');
+const gulpAutoPrefixer  = require('gulp-autoprefixer');
 // const gulpRename = require('gulp-rename')
 gulp.task('js',()=>{
 	gulp.src('static/js/*.js')
 	.pipe(gulpBabel({
 		presets:['es2015']
 	}))
-	.pipe(gulpWebpack({
-		output:{
-			filename:[name].js
-		},
-		stats:{
-			color:true
-		}
-	}))
 	.pipe(gulpUglify())
 	.pipe(gulpRev())
 	.pipe(gulp.dest('../dist/js'))
-	.pipe(gulpRev.manifest({
-		merge:true
-	}))
-	.pipe(gulp.dest('manifest'))
+	.pipe(gulpRev.manifest())
+	.pipe(gulp.dest('manifest/js'))
 })
 gulp.task('css',()=>{
 	gulp.src('static/css/*.css')
 	.pipe(gulpCssMin())
+	.pipe(gulpAutoPrefixer())
 	.pipe(gulpRev())
 	.pipe(gulp.dest('../dist/css'))
-	.pipe(gulpRev.manifest({
-		merge:true
-	}))
-	.pipe(gulp.dest('manifest'))
+	.pipe(gulpRev.manifest())
+	.pipe(gulp.dest('manifest/css'))
 })
 gulp.task('html',()=>{
-	gulp.src(['manifest/*.json','view/**/*.html'])
+	gulp.src(['manifest/**/*.json','view/**/*.html'])
 	.pipe(gulpRevCollector())
 	.pipe(gulp.dest('../dist/view'))
 })
 gulp.task('clean',()=>{
-	gulp.src('../dist')
+	gulp.src(['../dist','manifest'])
 	.pipe(gulpClean({
 		force:true
 	}))
+})
+gulp.task('cssReload',()=>{
+	gulp.src('static/css/*.css')
+	.pipe(gulp.dest('static/css/*.css'))
+})
+gulp.task('watch',()=>{
+	gulpWatch('static/css/*.css',()=>{
+		gulp.run('cssReload')
+	})
 })
 
