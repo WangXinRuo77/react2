d02fd399467979012f4b975afc89922fe24c90d7
wangerniu <596864509@qq.com>
2017-09-27 13:44:43 +0800

    docs:add vue develop  env

--- webpack/config/plugin.webpack_dev_server.js
+++ webpack/config/plugin.webpack_dev_server.js
@@ -1,18 +1,47 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-	entry:path.resolve(__dirname,'../plugins/webpack-dev-server/src/main.js'),
+	entry:{
+		"app":path.resolve(__dirname,'../plugins/webpack-dev-server/src/main.js')		
+	},
 
-	devtool:"inline-source-map",
+	// devtool:"inline-source-map",
 
 	devServer:{
 		contentBase:path.resolve(__dirname,'../plugins/webpack-dev-server/src'),
 		compress:false,
-		port:9000
+		port:9000,
+		noInfo:true, 
+		inline:true,
 	},
-
+	plugins:[
+		new HtmlWebpackPlugin({
+			filename:path.resolve(__dirname,'../plugins/webpack-dev-server/src/index.html'),
+			template:path.resolve(__dirname,'../plugins/webpack-dev-server/src/index.html'),
+			jnject:false,
+		})
+	],
 	output:{
-		filename:"[name].[hash].[id].js",
-		path:path.resolve(__dirname,'../plugins/webpack-dev-server/')
+		filename:"[id].[chunkHash].js",
+		path:path.resolve(__dirname,'../plugins/webpack-dev-server/src')
+	},
+	resolve:{
+		extensions:['.js','.vue','.json']
+	},
+	module:{
+		rules:[
+			{
+				test:/\.vue$/g,
+				loader:"vue-loader"
+			},
+			{
+				test:/\.js$/g,
+				loader:"babel-loader",
+			},
+			{
+				test:/\.css$/g,
+				loader:"style-loader!css-loader"
+			}
+		]
 	}
 }
--- webpack/plugins/webpack-dev-server/src/App.vue
+++ webpack/plugins/webpack-dev-server/src/App.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="component-app" id="componentApp">
-		<p>{{ message }}</p>
+		<p >{{ message }}</p>
 		<input type="text" v-model="message" name="">
+		<br/>
+		<input type="button" class="btn" value="showMessage" @click="showMessage">
 	</div>
 </template>
 <script>
@@ -10,6 +12,35 @@
 			return{
 				message:"hello webpack-dev-server"
 			}
+		},
+		created(){
+		},
+		methods:{
+			showMessage(){
+				alert(this.message)
+			}
 		}
 	}
 </script>
+<style>
+	body
+	{
+		margin:0;padding: 0;
+		text-align: center;
+	}
+	input,
+	.btn
+	{
+		padding:8px 15px;
+		border:1px solid #ddd;
+		display: inline-block;
+		border-radius: 5px;
+		background: #fff;
+	}
+	p{
+		font-size: 14px;
+		margin:0;
+		padding:0;
+		margin:1em 0;
+	}
+</style>
