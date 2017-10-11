36b4bc295bd9d10e708213f50622522d8ca520a2
wangerniu <596864509@qq.com>
2017-10-11 20:01:19 +0800

    feat: add html-webpack-plugin file

---
+++ webpack/html-webpack-plugin/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+	entry:path.resolve(__dirname,'../plugins/html-webpack-plugin/src/index.js'),
+	output:{
+		filename:"[name].bundle.js",
+		path:path.resolve(__dirname,'../plugins/html-webpack-plugin/')
+	},
+	devtool:"inline-source-map",
+	module:{
+		rules:[
+			{
+				test:/\.js$/g,
+				loader:"babel-loader"
+			},
+			{
+				test:/\.css$/g,
+				loader:"style-loader!css-loader"
+			}
+		]
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title:"output html webpack plugin"
+		})
+	]
+}
