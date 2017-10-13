605aa6bd5748dd7a7c8b78bf1e507c36c7ae8b1d
wangerniu <596864509@qq.com>
2017-10-13 16:17:01 +0800

    docs: finish webpack-dev-server total config item and run it success

--- webpack/webpack-dev-server/webpack.config.js
+++ webpack/webpack-dev-server/webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
-
+const webpack = require('webpack');
 module.exports = {
 	entry:path.resolve(__dirname,'src/index.js'),
-	output:{
-		path:"/",
+	output:{ 
+		path:path.resolve(__dirname),
 		filename:"bundle.js",
 	},
 	devServer:{
 		//Provides the ability to execute custom middleware after all other middleware internally within the server.
 		after(app){
-			console.log(app)
+			// console.log(app)
 		},
 		
 		//This option allows you to whitelist services that are allowed to access the dev server.
@@ -28,7 +28,7 @@ module.exports = {
 		compress:true,
 		// Tell the server where to serve content from. This is only necessary if you want to serve static files. devServer.publicPath will be used to determine where the bundles should be served from, and takes precedence.
 		// By default it will use your current working directory to serve content, but you can modify this to another directory:
-		contentBase:"",
+		// contentBase:"",
 
 		/*
 			lazy:true,
@@ -44,7 +44,7 @@ module.exports = {
 		historyApiFallback:true,
 
 		// Specify a host to use. By default this is localhost. If you want your server to be accessible externally, specify it like this:
-		host:"1,2,3,4",
+		// host:"1,2,3,4",
 
 		// Enable webpack's Hot Module Replacement feature
 		hot:true,
@@ -64,7 +64,7 @@ module.exports = {
 		*/
 
 		// oggle between the dev-server's two different modes. By default the application will be served with inline mode enabled. This means that a script will be inserted in your bundle to take care of live reloading, and build messages will appear in the browser console.
-		inline:false,
+		inline:true,
 
 		// With noInfo enabled, messages like the webpack bundle information that is shown when starting up and after each save, will be hidden. Errors and warnings will still be shown.
 		noInfo:true,
@@ -86,18 +86,18 @@ module.exports = {
 		*/
 
 		// Specify a port number to listen for requests on:
-		port:8088
+		port:8088,
 
 		// Proxying some URLs can be useful when you have a separate API backend development server and you want to send API requests on the same domain.
 		proxy: {
-			"/user":{
+			"/user": {
 				target:"http://localhost:3000",
 				secure:false,
 				pathRewrite: {
 					"^/api" : ""
 				}
 			}
-		}
+		},
 
 		// When using inline mode and you're proxying dev-server, the inline client script does not always know where to connect to. It will try to guess the URL of the server based on window.location, but if that fails you'll need to use this.
 		/*
@@ -109,7 +109,7 @@ module.exports = {
 			Imagine that the server is running under http://localhost:8080 and output.filename is set to bundle.js. By default the publicPath is "/", so your bundle is available as http://localhost:8080/bundle.js.
 			The publicPath can be changed so the bundle is put in a directory:
 		*/ 
-		publicPath: "/my",
+		// publicPath: "/my",
 
 		// With quiet enabled, nothing except the initial startup information will be written to the console. This also means that errors or warnings from webpack are not visible.
 		quiet: true,
@@ -120,7 +120,7 @@ module.exports = {
 		// It is possible to configure advanced options for serving static files from contentBase. See the Express documentation for the possible options. 
 		staticOptions: {
 
-		}	
+		},	
 
 		// This option lets you precisely control what bundle information gets displayed. This can be a nice middle ground if you want some bundle information, but not all of it.To show only errors in your bundle:
 		// This option has no effect when used with quiet or noInfo.
@@ -129,13 +129,16 @@ module.exports = {
 	module:{
 		loaders:[
 			{
-				test:/\.js/,
-				exclude:/node_modules/,
-				loader:"babel-loader",
-				query:{
-					presets:['react','es2015']
-				}
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 }
