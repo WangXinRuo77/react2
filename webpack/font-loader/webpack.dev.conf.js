efaf23c6c54de446cccc94e937e3472137a96a97
wangerniu <596864509@qq.com>
2017-10-14 20:25:27 +0800

    feat: use file-loader to resolve iconfont

--- webpack/font-loader/webpack.dev.conf.js
+++ webpack/font-loader/webpack.dev.conf.js
@@ -1,1 +1,42 @@
-webpack.dev.conf.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry:path.resolve(__dirname,'src/index.js'),
+	output:{
+		path:path.resolve(__dirname,'dist'),
+		publicPath:"dist",
+		filename:"[id].[chunkHash].js",
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+				loader:"file-loader",
+				options:{
+					limit:10000,
+				}
+			},
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          limit: 10000,
+         }
+      },			
+			{
+				test:/\.css/,
+				loader:"style-loader!css-loader"
+			}
+		]
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			template:path.resolve(__dirname,'src/index.html'),
+			minify:{
+				removeComments:true,
+				collapseWhitespace:true
+			}
+		})
+	]
+}
