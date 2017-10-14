294dc5e44a60b0f3406d5f209a70adfc0a571eba
wangerniu <596864509@qq.com>
2017-10-14 20:38:33 +0800

    delete: delete useless loaders

--- webpack/loaders/jsx/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Index from '../../src/jsx/index.jsx';
-
-
-ReactDOM.render(<Index />,document.getElementById('div1'))
--- webpack/loaders/vue/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Message from '../../src/vue/index.vue';
-
-new Vue({
-    el:"#div1",
-    render: h =>h(Message)
-})
