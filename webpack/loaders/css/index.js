659fb9267c326d8f201efd5721fac99451618e1d
wangerniu <596864509@qq.com>
2017-10-14 17:38:48 +0800

    chiro: delete loaders/css and loaders/csspre

--- webpack/loaders/css/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-import '../../src/css/common.css';
-
-const oText = document.createElement('h1');
-oText.innerHTML = 'created by index.js';
-oText.className = 'fc-3 text-center fc-main fs-20'
-document.body.appendChild(oText);
