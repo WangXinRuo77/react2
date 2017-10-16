114fb4ec2ea78736d3081882c138d902461b19f8
wangerniu <596864509@qq.com>
2017-10-16 19:27:38 +0800

    feat: add webpack.conf.js and init readme.md

---
+++ webpack/react/config/webpack.dev.conf.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+// const WebpackDevServer = require('webpack-dev-server');
+const WebpackBaseConfig = require('./webpack.base.conf.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry:path.resolve(__dirname,'../src/idnex.jsx'),
+	output:{
+		path:__dirname,
+		publicPath:"/",
+		filename:"[id].[chunkHash].js",
+	},
+	module: {
+		loaders: [
+			// {
+			// 	test:/\.jsx?$/,
+			// 	loader:"babel-loader",
+			// 	query:{
+			// 		presets:["es2015",'react']
+			// 	}
+			// },
+		]
+	},
+	devServer:{
+		compress: true,
+		hot: true,
+		inline: true,
+		open: true,
+		port: 8088,
+		proxy: {
+
+		},
+		staticOptions: {
+
+		}
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			template:path.resolve(__dirname,'../src/index.html'),
+			minify: {
+				removeComments:true,
+				collapseWhitespace:true
+			}
+		})
+	]
+}
