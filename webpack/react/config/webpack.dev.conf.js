32047f345d060f0c8635c2b59840a19a74bd50e0
wangerniu <596864509@qq.com>
2017-10-17 15:08:58 +0800

    config: develop environment config file

--- webpack/react/config/webpack.dev.conf.js
+++ webpack/react/config/webpack.dev.conf.js
@@ -16,7 +16,7 @@ module.exports = {
 				test:/\.jsx?$/,
 				loader:"babel-loader",
 				exclude:/node_modules/,
-				
+
 				query:{
 					presets:["es2015",'react']
 				}
@@ -24,6 +24,10 @@ module.exports = {
 			{
 				test:/\.css/,
 				loader:"style-loader!css-loader",
+			},
+			{
+				test:/\.less/,
+				loader:"style-loader!css-loader!less-loader"
 			}
 		]
 	},
