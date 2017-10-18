1365aa16a1e7c130266487c57944cb9535bd592b
wangerniu <596864509@qq.com>
2017-10-18 21:15:40 +0800

    config: write router in src/index.js

--- webpack/react/src/index.js
+++ webpack/react/src/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 
-import App from './App.jsx';
+
+// ui theme 
+import 'antd/dist/antd.css'
+import '../ui/default.less';
+
+
+
+// iconfont
+import '../static/fonts/iconfont.css'
+
+import ComponentHeader from './components/header/header.jsx';
+import ComponentFooter from './components/footer/footer.jsx';
+
+import PageIndex from './pages/index/index.jsx';
+import PageContest from './pages/contest/index.jsx';
+import PageProcedure from './pages/procedure/index.jsx';
+import PageCase from './pages/case/index.jsx';
+import PageAbout from './pages/about/index.jsx'; 
+
 
 ReactDOM.render(
-	<App />,
+	<Router >
+		<div>
+			<ComponentHeader />
+			<Route exact path="/" component={ PageIndex }></Route>
+			<Route path="/contest" component={ PageContest }></Route>
+			<Route path="/procedure" component={ PageProcedure }></Route>
+			<Route path="/case" component={ PageCase }></Route>
+			<Route path="/about" component={ PageAbout }></Route> 
+			<ComponentFooter />
+		</div>
+	</Router>,
 	document.querySelector('#app')
 )
