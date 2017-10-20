943a94c690c20f507d0e25cefc5d8862801dab6d
wangerniu <596864509@qq.com>
2017-10-20 19:53:47 +0800

    feat: add procedure jsx file

---
+++ webpack/react/src/pages/procedure/customer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import "@style/page_procedure_index.less"; 
+
+export default class CustomerProcedure extends React.Component {
+	constructor(){
+		super() 
+	}
+
+	render(){
+		return (
+			<main className="component-customer">
+				<h1 class="text-center">this is customer in procedure</h1>
+			</main>
+		)
+	}
+}
+
--- webpack/react/src/pages/procedure/index.jsx
+++ webpack/react/src/pages/procedure/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
- 
+import PageProcedureCustomer from '@pages/procedure/customer.jsx';
+import PageProcedureDesigner from '@pages/procedure/designer.jsx';
+import { Link , Router, Route, HashRouter } from 'react-router-dom';
+import "@style/page_procedure_index.less"; 
 
 export default class Procedure extends React.Component {
 	constructor(){
-		super()
-		this.state = {
-			name:'hello Procedure'
-		}
+		super() 
 	}
 
 	render(){
 		return (
 			<main>
-				<h1 style={{height:'600px'}}>{this.state.name}</h1>		
+				<div className="page-procedure">
+					<div className="component-banner"></div>
+					<Route exact path="/procedure/customer" component={ PageProcedureCustomer }></Route>
+					<Route path="/procedure/designer" component={ PageProcedureDesigner }></Route>
+				</div> 	
 			</main>
 		)
 	}
