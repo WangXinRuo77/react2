16246d4c923564ea2712a97eadf0ac26f1db911b
wangerniu <596864509@qq.com>
2017-10-23 20:18:18 +0800

    update: finish account dom in launch user

--- webpack/react/src/pages/launch/index.jsx
+++ webpack/react/src/pages/launch/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
- 
+
+import "@style/page_launch_index.less"; 
 
 export default class Case extends React.Component {
 	constructor(){
@@ -11,34 +12,34 @@ export default class Case extends React.Component {
 	render(){
 		return (
 			<main className="page-launch">	
-				<h1>发布赛事，精于帮你找到满意的设计</h1>
+				<h1 className="title">发布赛事，精于帮你找到满意的设计</h1>
 				<div className="component-account">
-					<nav className="">
+					<nav className="nav">
 						<a className="item item-active">已有账号</a>
 						<a className="item">没有账号</a>
 					</nav>
-					<div className="">
-						<div className="">
-							<div className="account" >
-								<span>账号</span>
+					<div className="user">
+						<div className="signup">
+							<div className="item" >
+								<span className="text">账号</span>
 								<input type="text" placeholder="邮箱或手机" />
 							</div>
-							<div className="password">
-								<span>密码</span>
+							<div className="item">
+								<span className="text">密码</span>
 								<input type="password" placeholder="密码" />
 							</div>
 						</div>
-						<div className="">
-							<div className="account" >
-								<span>账号</span>
+						<div className="signin hide">
+							<div className="item" >
+								<span className="text">账号</span>
 								<input type="text" placeholder="邮箱或手机" />
 							</div>
-							<div className="password">
-								<span>密码</span>
+							<div className="item">
+								<span className="text">密码</span>
 								<input type="password" placeholder="密码" />
 							</div>
-							<div className="password">
-								<span>确认密码</span>
+							<div className="item">
+								<span className="text">确认密码</span>
 								<input type="password" placeholder="确认密码" />
 							</div>													
 						</div>
