4cf54d9f1e07e1feb11916eec17a1febb63b7985
wangerniu <596864509@qq.com>
2017-10-27 23:33:39 +0800

    update: luanch/index code

--- webpack/react/src/pages/launch/index.jsx
+++ webpack/react/src/pages/launch/index.jsx
@@ -21,15 +21,38 @@ export default class Case extends React.Component {
 	Switch(){
 
 	}
+	handleSubmit(e){
+		e.preventDefault();
+		this.props.form.valide
+	}	
 	render(){
+    const { getFieldDecorator } = this.props.form;
+
+    const LoginFrom = Form.create(
+    	<Form onSubmit={ this.handleSubmit }>
+	       <FormItem label="E-mail" hasFeedback>
+	          {
+	          	getFieldDecorator('email', {
+		            rules: [
+			            {
+			              type: 'email', message: 'The input is not valid E-mail!',
+			            }, 
+			            {
+			              required: true, message: 'Please input your E-mail!',
+			            }
+		            ],
+		          })( <Input /> )
+	        	}
+	        </FormItem>
+    	</Form>
+    )
+
 		return (
 			<main className="page-launch">
 				<h1 className="title">发布赛事，精于帮你找到满意的设计</h1>
 				<Tabs defaultActiveKey="1" onChange={ this.Switch }>
 			    <TabPane tab="Tab 1" key="1">
-			    	<Form>
-			    		<FormItem></
-			    	</Form>
+			    	<LoginFrom />
 			    </TabPane>
 			    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>			
 			  </Tabs>
