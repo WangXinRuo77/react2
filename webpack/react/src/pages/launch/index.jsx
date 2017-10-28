618bf1336cd4fc18792f3d00650125b197436bc2
wangerniu <596864509@qq.com>
2017-10-28 21:00:25 +0800

    feat:get the usage of antd form

--- webpack/react/src/pages/launch/index.jsx
+++ webpack/react/src/pages/launch/index.jsx
@@ -1,62 +1,39 @@
 import React from 'react';
-import { Tabs, Form  } from 'antd';
+import { Tabs, Form, Input } from 'antd';
 
-import "@style/page_launch_index.less"; 
+import "@style/page_launch_index.less";
 
 import Validate from '@utils/regexp.js'
 
 const TabPane = Tabs.TabPane;
 const FormItem = Form.Item;
-export default class Case extends React.Component {
-	constructor(){
-		super()
-		this.state = {
+class Launch extends React.Component {
+    constructor() {
+        super()
+        this.state = {
 
-		}
-	}
-	componentDidMount() {
-		// disabled submit button at beginning
-		// this.props.form.validateFields();
-	}
-	Switch(){
-
-	}
-	handleSubmit(e){
-		e.preventDefault();
-		this.props.form.valide
-	}	
-	render(){
-    const { getFieldDecorator } = this.props.form;
-
-    const LoginFrom = Form.create(
-    	<Form onSubmit={ this.handleSubmit }>
-	       <FormItem label="E-mail" hasFeedback>
-	          {
-	          	getFieldDecorator('email', {
-		            rules: [
-			            {
-			              type: 'email', message: 'The input is not valid E-mail!',
-			            }, 
-			            {
-			              required: true, message: 'Please input your E-mail!',
-			            }
-		            ],
-		          })( <Input /> )
-	        	}
-	        </FormItem>
-    	</Form>
-    )
-
-		return (
-			<main className="page-launch">
-				<h1 className="title">发布赛事，精于帮你找到满意的设计</h1>
-				<Tabs defaultActiveKey="1" onChange={ this.Switch }>
-			    <TabPane tab="Tab 1" key="1">
-			    	<LoginFrom />
-			    </TabPane>
-			    <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>			
-			  </Tabs>
-			</main>
-		)
-	}
+        }
+    }
+    componentDidMount() {
+        // disabled submit button at beginning
+        // this.props.form.validateFields();
+    }  
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return ( 
+            < main className = "page-launch" >
+            <h1 className = "title" > 发布赛事， 精于帮你找到满意的设计 </h1>  
+            <Tabs defaultActiveKey = "1" onChange = { this.Switch } >
+            <TabPane tab = "Tab 1" key = "1" >
+                <FormItem>
+                    <Input placeholder="账号" {...getFieldDecorator('account')} />
+                </FormItem>
+            </TabPane>  
+            <TabPane tab = "Tab 2" key = "2" > Content of Tab Pane 2 </TabPane>			 
+            </Tabs> 
+            </main>
+        )
+    }
 }
+
+export default Form.create()(Launch) 
