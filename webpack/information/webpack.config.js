95d2ce855de084f78826acfa484b6e014f40d82b
wangerniu <596864509@qq.com>
2017-11-02 20:49:09 +0800

    configure: webpack build conf

---
+++ webpack/information/webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry:path.resolve(__dirname,'src/index.js'),
+  output:{
+    filename:"[id].[chunkHash].js",
+    path:path.resolve(__dirname,'dist')
+  },
+  module:{
+    loaders: [
+      {
+        test:/\.js$/,
+        loaders:"babel-loader",
+        exclude:/node_modules/,
+      },
+      {
+        test:/\.css$/,
+        use:ExtractTextWebpackPlugin.extract({
+          fallback:"style-loader",
+          use:["css-loader"]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:path.resolve(__dirname,'src/index.html')
+    }),
+    new ExtractTextWebpackPlugin('[id].[chunkHash].css')
+  ]
+}
